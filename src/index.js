2c4e69f2c3d9de15eedc0da2c76d80ebff958bf8
yogeshjat23 <22bec116@nith.ac.in>
2025-03-29 13:27:23 +0530

    cl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> {/* ✅ Wrap App inside BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+{/* 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,4 +25,5 @@ root.render(
     <App />
   </React.StrictMode>
 
-);
+); 
+*/}
